4995d88b0480145fe7ef4acb464ab18c62bdbc97
Daniel Lutz <git@daniellutz.com.br>
2026-08-12 21:35:03 -0300

    feat: add status bar at the bottom of the screen

--- .config/openbox/themes/default/polybar/launch.sh
+++ .config/openbox/themes/default/polybar/launch.sh
@@ -35,7 +35,8 @@ launch_bar() {
 
 	# Launch the bar
 	for mon in $(polybar --list-monitors | cut -d":" -f1); do
-		MONITOR=$mon polybar -q main -c "$DIR"/config.ini &
+		MONITOR=$mon polybar -q main -c "$DIR"/config.ini & \
+		MONITOR=$mon polybar -q bottom -c "$DIR/config.ini" &
 	done
 }
 
